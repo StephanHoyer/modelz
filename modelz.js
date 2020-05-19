93bc60be9a379c5acc6bd08e243d5424becbf8fd
Stephan Hoyer <ste.hoyer@gmail.com>
2020-05-19 16:22:17 +0200

    Package updates

--- modelz.js
+++ modelz.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.modelz = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
   function noop() {}
 
@@ -294,5 +294,5 @@
 
   return modelz;
 
-}));
+})));
 //# sourceMappingURL=modelz.js.map
